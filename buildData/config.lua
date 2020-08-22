35ba83e6bb4fa892d18179deb5fa3c30f1a80341
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2020-08-22 11:36:02 +0530

    Create config.lua

---
+++ buildData/config.lua
@@ -0,0 +1,10 @@
+gameName = "Butterfly game!" --The name of your game
+packageName = "com.a.valid.package.name" --A package name, see https://www.aftvnews.com/how-to-determine-the-package-name-of-an-android-app/
+versionCode = "1" --Version code of your game, a "int"
+versionSemn = "1.0' --Verions semtics 
+
+--[
+
+--]
+
+o
