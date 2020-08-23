dd9e9570df3359cbb2b95aa13c46dfe0e70aca9f
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2020-08-23 14:10:37 +0530

    Update config.lua

--- buildData/config.lua
+++ buildData/config.lua
@@ -10,6 +10,7 @@ Here are what the variables mean. You just have to change their values:
  versionCode is the internal APK version code.
  versionSemantic is the readable APK version string. Example: 2.1.0
  gameName is your game name which is going to be displayed in home screen/launcher.
+ Set usesMicrophone to true if your program needs it
 
 You can do a google search about any of these if feeling confused
 --]]
